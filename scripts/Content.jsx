b8c82aa9d49d8c0e05864fd1e6285be087b9134f
dhvani <dmm77@njit.edu>
2020-10-27 22:13:14 +0000

    fixed eslint errors on main and content

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -1,71 +1,64 @@
 import * as React from 'react';
+import ChatBox from './ChatBox';
 
 import { Socket } from './Socket';
 import { Users } from './Users';
-import { ChatBox } from './ChatBox';
+
 import { GoogleButton } from './GoogleButton';
 
-export function Content() {
-    
-    /* sends chat message to server */
-    function sendChat(event) {
-        let mssg = document.getElementById("typeinput");
-        Socket.emit('message channel', {'mssg':mssg.value});
-        
-        console.log('message sent to server');
-        
-        event.preventDefault();
-        mssg.value = "";
-        
-    }
-    
-    const [status, setstatus] = React.useState(false);
-    
-    function updateStatus() {
-        React.useEffect( () => {
-            Socket.on('user auth channel', data => {
-                setstatus(data['auth']);
-                console.log("status data", data['auth']);
-            });
-            return () => {
-                Socket.off('user auth channel');
-            };
-        });
-        
-        console.log("status", status);
-    }
-    
-    updateStatus();
-    
-    return (
-        <div>
-            
-          
-            <div class="header">
-                <Users />
+export default function Content() {
+  /* sends chat message to server */
+  function sendChat(event) {
+    const mssg = document.getElementById('typeinput');
+    Socket.emit('message channel', { mssg: mssg.value });
+
+    event.preventDefault();
+    mssg.value = '';
+  }
+
+  const [status, setstatus] = React.useState(false);
+
+  function updateStatus() {
+    React.useEffect(() => {
+      Socket.on('user auth channel', (data) => {
+        setstatus(data.auth);
+      });
+      return () => {
+        Socket.off('user auth channel');
+      };
+    });
+  }
+
+  updateStatus();
+
+  return (
+    <div>
+
+      <div className="header">
+        <Users />
+      </div>
+
+      { status === false ? <GoogleButton />
+
+        : (
+          <div>
+
+            <div className="chatbox">
+
+              <ChatBox />
+
             </div>
-            
-            { status == false ? <GoogleButton /> :  
-            
-            <div>
-             
-            <div class="chatbox">
-            
-                <ChatBox />
-                
+
+            <div className="form">
+              <form onSubmit={sendChat}>
+                <input id="typeinput" type="text" placeholder="Type a message here..." maxLength="280" />
+                <input id="submit" type="submit" />
+              </form>
             </div>
-            
-            <div class="form">
-                <form onSubmit={sendChat}>
-                    <input id="typeinput" type="text" placeholder="Type a message here..." maxlength="280"></input>
-                    <input id="submit" type="submit"></input>
-                </form>
-            </div>
-            
-            </div>
-            
-            }
-            
-        </div>
-    );
+
+          </div>
+        )}
+
+    </div>
+  );
 }
