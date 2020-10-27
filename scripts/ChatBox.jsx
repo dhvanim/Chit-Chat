99ac2dce20af34c05de96d9bf1260dc9bec3b2de
dhvani <dmm77@njit.edu>
2020-10-27 22:00:53 +0000

    fixed chatbox eslint errors

--- scripts/ChatBox.jsx
+++ scripts/ChatBox.jsx
@@ -1,9 +1,28 @@
 import * as React from 'react';
 import { Socket } from './Socket';
 
-export function ChatBox() {
+export default function ChatBox() {
   const [messages, setmessages] = React.useState([]);
   const [username, setusername] = React.useState('');
+
+  /* updates chat log */
+  function updateMessages(data) {
+    const chatlog = data.chat_log;
+    const { timestamp } = data;
+
+    /* user status or error message */
+    if (timestamp === '') {
+      setmessages((messages) => messages.concat(chatlog));
+      return null;
+    }
+
+    /* iterates through recieved chatlog and appends to message state */
+    chatlog.map((item) => {
+      setmessages((messages) => messages.concat(item));
+    });
+
+    return null;
+  }
 
   /* turns on socket channel for chat log and calls updatemessages */
   function getNewMessages() {
@@ -15,27 +34,11 @@ export function ChatBox() {
     });
   }
 
-  /* updates chat log */
-  function updateMessages(data) {
-    const chatlog = data.chat_log;
-    const { timestamp } = data;
-    console.log('Recieved messages from server: ', Object.keys(chatlog).length);
-    console.log('Recieved timestamp: ', timestamp);
-
-    /* user status or error message */
-    if (timestamp == '') {
-      console.log(chatlog);
-      setmessages((messages) => messages.concat(chatlog));
-      return null;
+  /* saves only first username recieved (its own) */
+  function updateUsername(data) {
+    if (username === '') {
+      setusername(data.username);
     }
-
-    /* iterates through recieved chatlog and appends to message state */
-    chatlog.map((item) => {
-      console.log(item);
-      setmessages((messages) => messages.concat(item));
-    });
-
-    console.log('End of recieved messages.');
   }
 
   function getUsername() {
@@ -45,14 +48,6 @@ export function ChatBox() {
         Socket.off('username channel', updateUsername);
       };
     });
-  }
-
-  /* saves only first username recieved (its own) */
-  function updateUsername(data) {
-    console.log('Recieved username from server: ', data.username);
-    if (username == '') {
-      setusername(data.username);
-    }
   }
 
   /* scroll bar */
@@ -67,23 +62,23 @@ export function ChatBox() {
 
   /* uses this class to format message (alignment/color) */
   function getLIClass(id) {
-    if (id == 'chit-chat-bot') {
+    if (id === 'chit-chat-bot') {
       return 'botuser';
     }
-    if (id == username) {
+    if (id === username) {
       return 'thisuser';
     }
-    if (id == '') {
+    if (id === '') {
       return 'entereduser';
     }
     return 'user';
   }
 
   /* format based on text/images/links */
-  function message_type(mssg, mssg_type) {
-    if (mssg_type == 'link') {
-      return <span className="message"><a className="message_link" target="_blank" href={mssg}>{mssg}</a></span>;
-    } if (mssg_type == 'image') {
+  function message_type(mssg, mssgType) {
+    if (mssgType === 'link') {
+      return <span className="message"><a className="message_link" target="_blank" rel="noreferrer" href={mssg}>{mssg}</a></span>;
+    } if (mssgType === 'image') {
       return <span className="message"><img className="message_image" src={mssg} alt="Error: Image could not be displayed" /></span>;
     }
     return <span className="message">{mssg}</span>;
@@ -100,7 +95,7 @@ export function ChatBox() {
       { messages.map((message, index) => (
         <li key={index} className={getLIClass(message.username)}>
 
-          { message.username == '' ? (
+          { message.username === '' ? (
             <span className="message">
               {' '}
               <br />
@@ -111,15 +106,15 @@ export function ChatBox() {
               {' '}
             </span>
           )
-
             : (
               <span>
-                <img className="usericon" src={message.icon} />
+                <img className="usericon" src={message.icon} alt="user icon" />
                 <span className="userid">
                   {message.username}
                   {' '}
                   <span className="auth">
-                    via{message.auth}
+                    via
+                    {message.auth}
                   </span>
                 </span>
                 {' '}
